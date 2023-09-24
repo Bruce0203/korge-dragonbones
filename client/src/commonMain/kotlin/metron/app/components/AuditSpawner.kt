e7c63404ed62562d9206ca890dbe5cd8a2308b78
Bruce0203 <BruceFreedy@gmail.com>
2023-09-24 19:12:06 +0900

    init

--- client/src/commonMain/kotlin/metron/app/components/AuditSpawner.kt
+++
@@ -1,9 +0,0 @@
-package metron.app.components
-
-import com.github.quillraven.fleks.*
-import metron.util.*
-
-class AuditSpawner : Component<AuditSpawner> {
-    override fun type() = Companion
-    companion object : ComponentHooks<AuditSpawner>()
-}
